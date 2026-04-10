be05b1f4b0843e1019f7b61c258d62c27e12b561
Krishna-Gupta17 <2024021228@mmmut.ac.in>
2026-04-10 12:32:13 +0530

    10 second delay fixed using ping pong

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,6 +2,15 @@ import app from './app.js'
 
 const port = process.env.PORT || 4000
 
+app.get('/ping', (req, res) => {
+  res.send('pong')
+});
+
+//listens to port for req 
 app.listen(port, () => {
-  console.log(`Backend API running on port ${port}`)
-})
+  setInterval(() => {
+    fetch('https://jera-backend.onrender.com/ping')
+      .then(() => console.log('Pinged self!'))
+      .catch(() => console.log('Self ping failed.'));
+  }, 1000 * 60 * 10);
+});
